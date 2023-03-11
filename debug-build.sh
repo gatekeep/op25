bce528f3fcd372649df5807129320d95ce0b7d2b
Bryan Biedenkapp <gatekeep@gmail.com>
2023-03-11 10:43:54 -0500

    use 4 cores for compiliation by default;

--- debug-build.sh
+++ debug-build.sh
@@ -3,6 +3,6 @@ git pull
 cd build
 rm -rf *
 cmake ../ -DCMAKE_BUILD_TYPE=Debug 2>&1 | tee cmake.log
-make                               2>&1 | tee make.log
+make -j4                           2>&1 | tee make.log
 sudo make install                  2>&1 | tee install.log
 sudo ldconfig
